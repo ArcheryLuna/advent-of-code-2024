e57606bfdf4e3e7b22b1efa740ee4dbc0f0b27b9
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2024-12-03 13:53:25 +0000

    Delete day1/target directory

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/archeryluna/Documents/advent-of-code/day1/target/debug/day1: /Users/archeryluna/Documents/advent-of-code/day1/src/main.rs
--- day1/target/debug/deps/day1-02b3fb9def4288bd.d
+++
@@ -1,5 +0,0 @@
-/Users/archeryluna/Documents/advent-of-code/day1/target/debug/deps/day1-02b3fb9def4288bd: src/main.rs
-
-/Users/archeryluna/Documents/advent-of-code/day1/target/debug/deps/day1-02b3fb9def4288bd.d: src/main.rs
-
-src/main.rs:
